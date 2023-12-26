a2b51d5f611df6f0c7504660e77304ba92144d4c
hippyhippohops <akravi923@gmail.com>
2023-12-26 17:17:55 -0600

    Update BengalCornering.m

--- BengalCornering.m
+++ BengalCornering.m
@@ -1,19 +1,7 @@
-%% An Introduction
-
-% all race cars negotiate corners, and the ability to do so as fast as
-% possible minimizes lap time. This chapter covers the physics involved in
-% cornering and how the cornering capability of a race car can be
-% investigated using data from the logging system.
+%% Scripts Dealing with Cornering
 
 
 %% Driver Cornering Sequence
-
-% The car–driver combination goes through various phases when taking a
-% corner. The cornering process basically consists of the following phases:
-% Braking Point to Initial Turn-in Point, . Turn-in Point to Corner,s Apex,
-% Corner Exit. The following figures are to illustrates the different
-% events taking place during cornering with the driver activity channels
-% (throttle, brake pedal position, and steering wheel).
 
 %Picking out the relevant data we want
 FSAE = readtable('cleaned.csv');
@@ -31,25 +19,7 @@ FSAE(:,'SteeringAngle');
 % use { } brackets instead of ( ). So I use the command like 
 % plot(FSAE{:,'Time'},FSAE{:,'ThrottlePosition'});
 
-figure;
-hold on;
-plot(FSAE{:,'Time'},FSAE{:,'ThrottlePosition'});
-hold on;
-plot(FSAE{:,'Time'},FSAE{:,'BrakePressureFront'});
-hold on;
-plot(FSAE{:,'Time'},FSAE{:,'BrakePressureRear'});
-hold on;
-plot(FSAE{:,'Time'},FSAE{:,'WheelSpeedFL'});
-hold on;
-plot(FSAE{:,'Time'},FSAE{:,'WheelSpeedFR'});
-hold on;
-plot(FSAE{:,'Time'},FSAE{:,'WheelSpeedRL'});
-hold on;
-plot(FSAE{:,'Time'},FSAE{:,'WheelSpeedRR'});
-hold on;
-plot(FSAE{:,'Time'},FSAE{:,'SteeringAngle'});
-title('Comparison of Brake Pressure, Wheel Speed & Throttle Position against Time');
-
+% We want to see how each variable acts with respect to time. 
 figure;
 plot(FSAE{:,'Time'},FSAE{:,'ThrottlePosition'});
 title('Plot Throttle Position against Time');
@@ -75,6 +45,84 @@ figure;
 plot(FSAE{:,'Time'},FSAE{:,'SteeringAngle'});
 title('Plot Steering Angle against Time');
 
+% Also, now we want to study the relationship between some variables that
+% act in tandem.
+
+figure; 
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'BrakePressureFront'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'BrakePressureRear'});
+title('Relationship between Front and Rear Brake Pressures against Time');
+
+figure; 
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'WheelSpeedFL'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'WheelSpeedFR'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'WheelSpeedRL'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'WheelSpeedRR'});
+title('Relationship between Wheel Speed (each tire) against Time');
+
+%Here, I wanted to plot the brake pressure (front & back), steered angle
+%and wheel (for each wheel) to identify the turning points. But after
+%plotting it, I realise it is hard to do it visually. So, it is better to
+%come up with an algorithm to identify the timestamps when the turning is
+%occuring and at each interval are we turning left or right. 
+figure;
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'ThrottlePosition'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'BrakePressureFront'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'BrakePressureRear'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'WheelSpeedFL'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'SteeringAngle'});
+title('Comparison of Brake Pressure, Wheel Speed (FL) & Throttle Position against Time');
+
+figure;
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'ThrottlePosition'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'BrakePressureFront'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'BrakePressureRear'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'WheelSpeedFR'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'SteeringAngle'});
+title('Chartering of Brake Pressure, Wheel Speed (FR) & Throttle Position against Time');
+
+figure;
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'ThrottlePosition'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'BrakePressureFront'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'BrakePressureRear'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'WheelSpeedRL'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'SteeringAngle'});
+title('Chartering of Brake Pressure, Wheel Speed (RL) & Throttle Position against Time');
+
+figure;
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'ThrottlePosition'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'BrakePressureFront'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'BrakePressureRear'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'WheelSpeedRR'});
+hold on;
+plot(FSAE{:,'Time'},FSAE{:,'SteeringAngle'});
+title('Chartering of Brake Pressure, Wheel Speed (RR) & Throttle Position against Time');
+
 % ROOM FOR IMPROVEMENT 1: WHILE THE ABOVE PICTURE IS ALL GOOD, it would be
 % better to slice the timeline into pieces where the steered angle changes 
 % from 0 and returns to 0, so that we actually identify when the car turns.
@@ -84,46 +132,122 @@ title('Plot Steering Angle against Time');
 % first. I guess working on identifying corners should be the first
 % business we should be concerned about. 
 
-% ROOM FOR IMPROVEMENT 2: Find a way to calculate slip angle, then we can 
-% plot slip angle against lateral force for each wheel, this will allow us 
-% to identify the linear range, transitional range and frictional range 
-% (page 141). 
+%% Now, we calculate the slip angle and plot the lateral force against the slip angle
 
-%The formula is derived from Race Car Vehicular Dynamics and I took this
-%from page 148
+% CALCULATE SLIP ANGLE FOR EACH TIRE
 
-slip_angle = [];
+% To calculate the slip angle of the front of the car, alpha_f, we need the
+% following data: lateral velocity (v), CG location (a) - [In this case,we
+% assume  that laterak velocity is just 1, but FIND OUT ACTUAL CG
+% LOCATION], Yawing velocity (r) - [In this case,we assume  that yawing
+% velocity is just rotational velocity about the Z axis, from reddit: Yaw
+% Rate and Slip Angles - CHECK IF IT IS TRUE], vehicle velocity (V), and
+% steer angle front wheels (delta). 
 
-%In order to calculate slip angle I need lateral velocity first i guess
+% Slip Angle - Front Left
 
-%Calculating Front Left Slip Angle
+v = FSAE{:,'GForceLat'};
+a = 1;
+r = FSAE{:,'AccelerationZ'};
+V = FSAE{:,'WheelSpeedFL'};
+delta = FSAE{:,'SteeringAngle'};
 
+slip_angle_front_left = [];
 
-for i=1:length(FSAE{:,'Time'});
-    %we will calculate the slip angle of each vehicle at each time
-    %instance, store the value and then plot it
-    0;
+for i=1:length(v)
+    slip_angle_front_left(i) = v(i)/V(i) + (a*r(i))/V(i) - delta(i);
 end
+
+figure;
+plot(FSAE{:,'Time'},slip_angle_front_left);
+title('Plot of Slip Angle of the Front Left Wheel against Time');
+%FIGURE OUT WHY THE OUTPUT IS SO WEIRD
+
+% Slip Angle - Front Right
+
+v = FSAE{:,'GForceLat'};
+a = 1;
+r = FSAE{:,'AccelerationZ'};
+V = FSAE{:,'WheelSpeedFR'};
+delta = FSAE{:,'SteeringAngle'};
+
+slip_angle_front_right = [];
+
+for i=1:length(v)
+    slip_angle_front_right(i) = v(i)/V(i) + (a*r(i))/V(i) - delta(i);
+end
+
+figure;
+plot(FSAE{:,'Time'},slip_angle_front_right);
+title('Plot of Slip Angle of Front Right Wheel against Time');
+%FIGURE OUT WHY THE OUTPUT IS SO WEIRD
+
+%NOTE THAT THE FORMULA FOR SLIP ANGLE IS DIFF FROM FRONT & REAR
+
+% Slip Angle - Rear Left
+
+v = FSAE{:,'GForceLat'};
+a = 1; % We use b for the rear formula
+b = 1;
+r = FSAE{:,'AccelerationZ'};
+V = FSAE{:,'WheelSpeedRL'};
+
+slip_angle_rear_left = [];
+
+for i=1:length(v)
+    slip_angle_rear_left(i) = v(i)/V(i) + (b*r(i))/V(i) ;
+end
+
+figure;
+plot(FSAE{:,'Time'},slip_angle_rear_left);
+title('Plot of Slip Angle of Rear Left Wheel against Time');
+%FIGURE OUT WHY THE OUTPUT IS SO WEIRD
+
+% Slip Angle - Rear Right
+
+v = FSAE{:,'GForceLat'};
+a = 1;
+b = 1;
+r = FSAE{:,'AccelerationZ'};
+V = FSAE{:,'WheelSpeedRR'};
+delta = FSAE{:,'SteeringAngle'};
+
+slip_angle_rear_right = [];
+
+for i=1:length(v)
+    slip_angle_rear_right(i) = v(i)/V(i) + (b*r(i))/V(i);
+end
+
+figure;
+plot(FSAE{:,'Time'},slip_angle_rear_right);
+title('Plot of Slip Angle of Rear Right Wheel against Time');
+%FIGURE OUT WHY THE OUTPUT IS SO WEIRD
+
+% Now, after plotting the slip angle against the lateral force for each
+% wheel
+
+figure;
+plot(slip_angle_front_left,FSAE{:,'GForceLat'});
+title('Plot of Slip Angle (FL) vs GForceLat')
+
+figure;
+plot(slip_angle_front_right,FSAE{:,'GForceLat'});
+title('Plot of Slip Angle (FR) vs GForceLat')
+
+figure;
+plot(slip_angle_rear_left,FSAE{:,'GForceLat'});
+title('Plot of Slip Angle (RL) vs GForceLat')
+
+figure;
+plot(slip_angle_rear_right,FSAE{:,'GForceLat'});
+title('Plot of Slip Angle (RR) vs GForceLat');
+
+% Note that the graphs look weird as I have not yet spliced the time. 
+
+
 
 %% Plotting the Traction Circle
 
-% How Longitudinal and Lateral acceleration shows cornering 
-% 1) When longitudinal acceleration is negative while lateral G is at zero, 
-% the car is braking in a straight line.
-% 2) At the point where the longitudinal deceleration is still more or less
-% the same, but the car has started to build up lateral Gs, the driver is 
-% braking into the corner.
-% 3) Then, Maximum lateral acceleration is reached and  the longitudinal 
-% deceleration is reduced to almost zero Gs.
-% 4) Lastly, when the car accelerating out of the corner, it is indicated 
-% with positive longitudinal acceleration and decreasing lateral Gs.
-
-% We can plot the Longitudinal and Lateral acceleration in a X-Y plot. This
-% is known as the "traction circle.
-% NOTE THAT A tire’s maximum grip in any direction depends on the vertical 
-% load to which it is being subjected. This implies that the size of the 
-% traction circle is not constant. In a high-downforce corner, the circle's
-% diameter is larger than in a slow corner. 
 figure;
 plot(FSAE{:,'GForceLat'},FSAE{:,'GForceLong'},'linestyle','none','marker','.');
 title('Plot of Longtidinual G vs Lateral G')
@@ -131,14 +255,7 @@ title('Plot of Longtidinual G vs Lateral G')
 % Friction circle diagrams are useful to evaluate if drivers are using the
 % full grip potential of the vehicle.
 
-% ROOM FOR IMPROVEMENT 3: Compare traction circle for different drivers 
-% page 144 from data analysis book
-
-% Now, that we ploted the g-g diagram, we want to analyze the cares 
-% cornering potential during transient phases, and this can be done by
-% calculating the car's combined G acceleration. It basically represents 
-% the radius of the vehicle s instantaneous traction circle. STUDY HOW THIS
-% IS DONE using this 2 tools!!!
+% We now want to calculate the combined acceleration (G). 
 
 G_comb = [];
 
@@ -158,22 +275,6 @@ hold on;
 %% EFFECTS OF SPEED
 
 
-% The traction circle and combined G are good working tools for
-% investigating the cornering potential of a race car. The vehicle s
-% traction circle radius is not constant. It varies with the total vertical
-% load acting on the car%s center of gravity. The most important parameter
-% here is aerodynamic download, which is speed dependent.Therefore, a
-% relationship exists between the vehicle%s speed and its acceleration
-% potential, both in a longitudinal and lateral sense. 
-
-% Aerodynamic downforce increases proportional to the square of speed,
-% resulting in greater cornering and braking potential. Drag has the same
-% relationship to speed, so the more speed increases, the less power
-% remains to accelerate the vehicle. Now, we want to illustrate this.
-
-%First, we use the statistics summary from our data cleaning done earlier.
-% We know that the following summary for the vehicle speed
-
 max(FSAE{:,'VehicleSpeed'}); % Output = 76.4000
 min(FSAE{:,'VehicleSpeed'}); % Output = -0.1000
 
@@ -191,6 +292,17 @@ third_quantile = find(FSAE{:,'VehicleSpeed'} > 42.2000);
 length(first_quantile); % 24915
 length(second_quantile); % 49695
 length(third_quantile); % 25554
+%Now I will plot the quantile to ensure it is correct
+min(first_quantile);
+max(first_quantile);
+min(second_quantile);
+max(second_quantile);
+min(third_quantile);
+max(third_quantile);
+
+%The values here look all wrong, so fix them and redo the next part. Figure
+%out how I can cut the speed of the vehicle into 3 sections. Maybe I can
+%create a new speed array, sort the speed, and then cut it into 3 parts) 
 
 first_quantile_long_G = FSAE{:,'GForceLong'}(first_quantile);
 second_quantile_long_G = FSAE{:,'GForceLong'}(second_quantile);
@@ -249,69 +361,39 @@ title('Plot showing WheelSpeedRR against GForceLat and GForceLong')
 %Edit the graph more to look like what it does in Data Analysis Book Page
 %150 & 151
 
-%% Studying Steering
+%% Figure out a way to algorithmically study under and over steer of a vehicle based on section 7.4
 
-%DEFINITIONS
+%% Ackermann Steering Angle and it's Related Calculations
 
-% Vehicle balance is commonly described by the terms understeer, neutral 
-% steer, and oversteer. In a simple world, understeer causes the vehicle to
-% "push" its front tires to the outside of the corner, whereas oversteer 
-% causes the rear axle to break out. Neutral steer is the situation where 
-% neither understeer nor oversteer are present. 
-
-% Understeer and oversteer are vehicle dynamics terms used to describe the
-% sensitivity of a vehicle to steering. Oversteer is what occurs when a car
-% turns (steers) by more than the amount commanded by the driver.
-% Conversely, understeer is what occurs when a car steers less than the
-% amount commanded by the driver.
-
-% HOW TO ANALYSE STEERING
-
-% The most common way to analyze cornering balance is to look at the input
-% the vehicle acquires from the driver as a reaction to a handling problem.
-% Steering movement and pedal activities can reveal much about the handling
-% of the car. Steering angle, throttle position, and lateral acceleration
-% are the channels to watch.
-
-
+wheelbase = 60;
+R=[];
+for i=1:length(FSAE{:,"GForceLat"})
+    R(i) = (FSAE{:,"VehicleSpeed"}(i))^2 / FSAE{:,"GForceLat"}(i);
+end
+ackermann_steering_angle = [];
+for i=1:length(FSAE{:,"GForceLat"})
+    ackermann_steering_angle(i) =  wheelbase/R(i);
+end
 
 figure;
-plot(FSAE{:,'Time'},FSAE{:,'ThrottlePosition'});
-title('Throttle Position against Time');
+plot(FSAE{:,'Time'},ackermann_steering_angle);
+title('Plot Ackermann Steering Angle Against Time')
+
+%CARRY ON MORE CALCULATIONS HERE
+
+%% Yaw Balance
+R=[];
+for i=1:length(FSAE{:,"GForceLat"})
+    R(i) = (FSAE{:,"VehicleSpeed"}(i))^2 / FSAE{:,"GForceLat"}(i);
+end
+
+angular_velocity = [];
+for i=1:length(FSAE{:,"VehicleSpeed"})
+    angular_velocity(i) = FSAE{:,"VehicleSpeed"}(i)/R(i);
+end
+
 figure;
-plot(FSAE{:,'Time'},FSAE{:,'SteeringAngle'}); 
-title('Steering Angle against Time');
-figure;
-plot(FSAE{:,'Time'},FSAE{:,'GForceLat'});
-title('GForceLat against Time');
-
-% FOR OVERSTEERING
-
-% The driver counters oversteer by reducing the steering angle. The gray
-% line in the steering graph indicates approximately how the steering
-% movement should have been addressed. Steering corrections for oversteer
-% very often are accompanied by little dips in the lateral G graph,
-% parallel to the steering wheel movement. In general, lateral acceleration
-% levels are lower than expected. Oversteer creates a rough lateral G graph
-% as the car loses and regains grip. Variations smaller than 0.25 G and of
-% a shorter duration than 0.3 sec are caused by irregularities in the track
-% surface. Lateral G variations due to oversteer are confirrmed in the
-% steering angle graph.
-
-% Another oversteer indicator is when the vehicle is not willing to accept
-% full throttle. In this example, the driver waits until the corner is
-% completed before applying full throttle to avoid the rear stepping out.
-
-% FOR UNDERSTEERING
-
-% Understeer is a little more dif!cult to diagnose. Characteristic to
-% understeer is the excessive steering angle. When on corner entry the
-% steering angle gradually keeps increasing, we re dealing with understeer.
-% In this case the steering angle will often peak before the lateral G
-% trace. Mid-corner and corner exit understeer generally shows a gradual
-% decrease in the lateral G trace, while steering angle increases. In this
-% case maximum lateral acceleration will peak before the maximum steering
-% angle. Typical of mid-corner and corner exit understeer is the reluctance
-% of the car to accept full throttle.
+plot(FSAE{:,'Time'},angular_velocity);
+title("Plot Angular Velocity against Time");
 
 

7e90431452209f31b177a3d0b44acd4936160d93
hippyhippohops <akravi923@gmail.com>
2023-12-29 17:55:56 -0600

    Update WheelLoadsandWeightTransfer.m

--- WheelLoadsandWeightTransfer.m
+++ WheelLoadsandWeightTransfer.m
@@ -8,6 +8,8 @@ h_COG = 12; % h_COG denotes center of gravity height from ground in metres
 T = 45; % T denotes track weight in metres
 G_lat = FSAE{:,'GForceLat'}; % G_lat denotes lateral acceleration at CoG
 
+
+%Calculating Reacting Inertial Force
 F = [];
 for i=1:length(G_lat)
     F(i) = W * G_lat(i);
@@ -15,8 +17,10 @@ end
 
 figure;
 plot(FSAE{:,'Time'},F,'k');
-title('Force against Time');
+title('Reacting Inertial Force against Time');
 
+
+% Calculating Weight Transfer due to Cornering
 for i=1:length(G_lat)
     delta_W_lat(i) = (W * G_lat(i)*h_COG)/T;
 end
@@ -24,6 +28,9 @@ end
 figure;
 plot(FSAE{:,'Time'},delta_W_lat,'k');
 title('Total Lateral Weight Transfer against Time');
+%Once again, this will work out better once I identify the left and the
+%right turns. Then, plot those in order to see the lateral weight transfer
+%during actual turns. 
 
 delta_W_uF = []; %lateral_weight_transfer_unsprung_mass front
 delta_W_uR = []; %lateral_weight_transfer_unsprung_mass Rear
@@ -32,12 +39,12 @@ delta_W_uR = []; %lateral_weight_transfer_unsprung_mass Rear
 % weight), (ii) G_lat (we already have this), (iii) h_F, h_R (Front, Rear 
 % unsprung center of gravity height),(iv) T_F, T_R (Front/rear track width)
 
-W_uF = 13.15;
-W_uR = 14.06;
-h_F = 9;
-h_R = 12; 
-T_F = 45;
-T_R = 50; 
+W_uF = 13.15; %Change this after getting actual number
+W_uR = 14.06; %Change this after getting actual number
+h_F = 9; %Change this after getting actual number
+h_R = 12; %Change this after getting actual number
+T_F = 45; %Change this after getting actual number
+T_R = 50; %Change this after getting actual number
 
 for i=1:length(G_lat)
     delta_W_uF(i) = (W_uF * G_lat(i)*h_F)/T_F;
@@ -64,14 +71,14 @@ delta_W_gR = []; %lateral_geometric_weight_transfer_unsprung_mass Rear
 % (wheelbase) (v) h_RCF, h_RCR (Front, Rear roll center height from
 % ground),(iv) T_F, T_R (Front/rear track width)
 
-W_sF = 0;
-W_sR = 0;
-a = 0;
-WB = 1;
-h_RCF = 0;
-h_RCR = 0;
-T_F = 0;
-T_R = 0;
+W_sF = 1; %Change this after getting actual number
+W_sR = 1; %Change this after getting actual number
+a = 1; %Change this after getting actual number
+WB = 1; %Change this after getting actual number
+h_RCF = 1; %Change this after getting actual number
+h_RCR = 1; %Change this after getting actual number
+T_F = 1; %Change this after getting actual number
+T_R = 1; %Change this after getting actual number
 
 for i=1:length(G_lat)
     delta_W_gF(i) = (W_sF * G_lat(i)*(a/WB)*h_RCF)/T_F;
@@ -79,7 +86,7 @@ end
 
 figure;
 plot(FSAE{:,'Time'},delta_W_gF,'k');
-title('lateral_geometric_weight_transfer_unsprung_mass front against Time');
+title('Lateral Geometric Weight Transfer Unsprung Mass Front against Time');
 
 for i=1:length(G_lat)
     delta_W_gR(i) = (W_sR * G_lat(i)*((WB-a)/WB)*h_RCR)/T_R;
@@ -87,4 +94,56 @@ end
 
 figure;
 plot(FSAE{:,'Time'},delta_W_gR,'k');
-title('lateral_geometric_weight_transfer_unsprung_mass Rear against Time');
+title('Lateral Geometric Weight Transfer Unsprung Mass Rear against Time');
+
+% Calculating Sprung Mass Center of Gravity  (h_SCOG)
+
+h_COG = 12; %Change this after getting actual number
+h_F = 9; %Change this after getting actual number
+h_R = 12; %Change this after getting actual number
+h_RCF = 0; %Change this after getting actual number
+h_RCR = 0; %Change this after getting actual number
+b = 1; % I am not sure what b is. Find out what it is actually
+wheel_base = 60; %Change this after getting actual number
+
+h_SCOG = 2*h_COG - (h_F + h_R)/2;
+
+% Calculating h_roll
+
+h_roll = h_SCOG - h_RCF-(b/wheel_base)*(h_RCR-h_RCF);
+
+% Calculating front and rear roll gradient (RG_F and RG_R) in order to
+% calculate the suspension roll stiffness, (q)
+
+roll_angle_front = 10; % I am stating this as 10 for the sake of 
+% calculations. Change this after getting actual number 
+roll_angle_rear = 12; % I am stating this as 10 for the sake of 
+% calculations. Change this after getting actual number 
+
+RG_F = gradient(roll_angle_front)/gradient(FSAE{:,"GForceLat"});
+RG_R = gradient(roll_angle_rear)/gradient(FSAE{:,"GForceLat"});
+
+q = 0.2; % actual formula is but once I get the actual data add in this 
+% actual RG_R/(RG_R+RG_F)
+
+% COME BACK TO THIS ONCE I AM DONE WITH CHAPTER 9
+
+%% Longitudinal Weight Transfer
+
+h_COG = 12; %Change this after getting actual number
+wheel_base = 60; %Change this after getting actual number
+W = 200; % W denotes vehicle weight in Kg
+
+longitudinal_weight_transfer = [];
+
+for i=1:length(FSAE{:,'GForceLong'})
+    longitudinal_weight_transfer(i) = (W*FSAE{:,'GForceLong'}(i)*h_COG)/wheel_base;
+end
+
+figure;
+plot(FSAE{:,'Time'},longitudinal_weight_transfer);
+title("Plotting Longitudinal Weight Transfer against Time");
+
+%% Banking and Grade Effects
+
+% How to calculate banking angle???
